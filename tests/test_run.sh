c05d2947ec647dfc28c4c232fabb8b967e4ffdf1
Danda <danda@vthink.co.in>
2018-09-22 15:12:27 +0530

    aa

--- tests/test_run.sh
+++ tests/test_run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 cd "$TRAVIS_BUILD_DIR/tests"
-newman run bitcoinz.postman_collection2.json -r cli,html
+#newman run bitcoinz.postman_collection2.json -r cli,html
+
+
+#newman run <collection json file> --reporters cli,html --reporter-html-export <location to export>>\report.html
+
+newman run bitcoinz.postman_collection2.json --reporters cli,html --reporter-html-export report.html
